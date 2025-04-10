3229c793bf320d27b1a94bb7271fb5b1a8f83dbf
Victoire07 <victoireboutin07@gmail.com>
2025-04-10 15:15:56 +0200

    Sauvegarde : création 1ère boucle et initialisation de variables

---
+++ 2-selection_sort.c
@@ -0,0 +1,19 @@
+#include "sort.h"
+/**
+ * selection sort -  Trie un tableau d'entiers dans l'ordre croissant
+ * @array: Pointeur vers le tableau
+ * @size: Nombre d'éléments dans le tableau
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t minimum_index = i;
+	size_t j;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		size_t minimum_index = i;
+	}
+
+}
