4693f699770c12c75363d215726ad37e008e7b6b
Victoire07 <victoireboutin07@gmail.com>
2025-04-10 03:08:04 +0200

    Sauvegarde Task 0 : début 1ere boucle

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,7 +6,14 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	if (array == NULL)
+	size_t i;
+
+	if (array == NULL || size <= 1)
 		return;
+
+		for (i; i < size - 1 - i; i++)
+		{
+
+		}
 
 }
