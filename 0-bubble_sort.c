0cfb17bdb0bec17849aa248397caeb6a8cbf1651
Victoire07 <victoireboutin07@gmail.com>
2025-04-10 22:57:30 +0200

    Améliorationn du fichier  avec arrêt anticipé(break) si tableau déjà trié

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,22 +9,28 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	size_t j;
 	int temporaire;
+	int echange;
 
 	if (array == NULL || size <= 1)
 		return;
 
 		for (i = 0; i < size - 1; i++)
 		{
+			echange = 0;
+
 			for (j = 0; j < size - 1 - i; j++)
 			{
 				if (array[j] > array[j + 1])
 				{
+					echange = 1;
 					temporaire = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temporaire;
 					print_array(array, size);
 				}
 			}
+			if (echange == 0)
+			break;
 
 		}
 }
