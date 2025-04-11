942f383d1cc4274d827de631d0601e2856137a79
Victoire07 <victoireboutin07@gmail.com>
2025-04-11 14:30:33 +0200

    Modification Betty Style

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -17,7 +17,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		minimum_index = i;
-		
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[minimum_index])
