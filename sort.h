e6e16604b9c521c8cd126be39c9c6880ae987660
Victoire07 <victoireboutin07@gmail.com>
2025-04-11 15:49:35 +0200

    ajout prototype Karim question 3

--- sort.h
+++ sort.h
@@ -21,6 +21,7 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 /* fonctions fournies utiles pour les tasks */
 void print_array(const int *array, size_t size);
