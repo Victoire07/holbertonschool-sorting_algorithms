f656062369e0012b8f160061c8120d47a75d42a1
Victoire07 <victoireboutin07@gmail.com>
2025-04-11 15:08:01 +0200

    Modification code 0 pour passation points checker

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,20 +9,17 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	size_t j;
 	int temporaire;
-	int echange;
 
 	if (array == NULL || size <= 1)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		echange = 0;
 
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				echange = 1;
 				temporaire = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temporaire;
@@ -30,7 +27,5 @@ void bubble_sort(int *array, size_t size)
 			}
 
 		}
-		if (echange == 0)
-		break;
 	}
 }
