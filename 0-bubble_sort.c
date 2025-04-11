c16ecb084c6033d4af701956d846a4208b711bd9
Victoire07 <victoireboutin07@gmail.com>
2025-04-11 14:05:16 +0200

    Mise a jour des tabulations pour erreur Betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,18 +18,19 @@ void bubble_sort(int *array, size_t size)
 	{
 		echange = 0;
 
-	for (j = 0; j < size - 1 - i; j++)
-	{
-		if (array[j] > array[j + 1])
+		for (j = 0; j < size - 1 - i; j++)
 		{
+			if (array[j] > array[j + 1])
+			{
 			echange = 1;
 			temporaire = array[j];
 			array[j] = array[j + 1];
 			array[j + 1] = temporaire;
 			print_array(array, size);
+			}
+		
+		if (echange == 0)
+			break;
 		}
-	}
-	if (echange == 0)
-		break;
 	}
 }
